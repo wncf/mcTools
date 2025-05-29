840bcc1decf87b6efec3dbf7f37fd0006adb9e5e
ljt <wonichengfeng@tzh7.wecom.work>
2025-05-29 18:33:16 +0800

    改为hash路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import baseConfig from "@/config/base.config";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
